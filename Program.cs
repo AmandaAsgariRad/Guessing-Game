01599a5e2cc188b9848c07afec46dd9080973345
Amanda Asgari-Rad <amanda_asgarirad@yahoo.com>
2023-05-23 15:44:56 -0500

    full program

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess your secret number");
+
+            int SecretNumber = 42;
+            int guessAllowance = 4;
+            int userTries = 0;
+
+            while (userTries < guessAllowance)
+            {
+                int response = int.Parse(Console.ReadLine());
+                userTries++;
+
+                if (response == SecretNumber)
+                {
+                    Console.WriteLine("Your guess is correct!");
+                    break;
+                }
+                else if (userTries < guessAllowance)
+                {
+                    Console.WriteLine($"This is your {userTries} guess. Guess again");
+                }
+                else
+                {
+                    Console.WriteLine("You failed");
+                }
+
+            }
+        }
+    }
+}
+
+//for (int i=0; i < 4; i++)
